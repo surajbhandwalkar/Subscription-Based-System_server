c088bfe77f037c15568e28d39d28fb4a1151291a
suraj bhandwalkar <suraj.b9930@gmail.com>
2025-05-29 19:08:13 +0530

    Added user model and API to save user data

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-//const mongoose = require("mongoose");
+
 const cors = require("cors");
 
 require('dotenv').config();
@@ -10,10 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
